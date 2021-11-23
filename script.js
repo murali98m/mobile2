f1de6567b2343bf6ab53a0e7f04977424daa6de4
murali98m <muralidharan98m@gmail.com>
2021-11-23 18:31:56 +0530

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-var i;
+var add=function a(x,y) {
+    var z= x+y;
+    console.log(z)
+    
+}
+var sub=function a(x,y) {
+    var z= x-y;
+    console.log(z)
+    
+}
+var mul=function a(x,y) {
+    var z= x*y;
+    console.log(z)
+    
+}
+var div=function a(x,y) {
+    var z= x/y;
+    console.log(z)
+    
+}
+add(3,5);
+sub(3,5);
+mul(3,5);
+div(3,5);
 
-    for(i=1;i<=10;i++)
-    {
-        console.log(i);
-    }
